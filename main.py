37bf91df15f7a28afae56e13b4069f901501087d
Nejc Skornšek <40896702+nacoraco@users.noreply.github.com>
2024-04-28 22:47:40 +0200

    SQLite

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from typing import Union
-
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
+# Define a function to connect to the SQLite database
+def get_db_conn():
+    conn = sqlite3.connect('FastAPI.db')
+    conn.row_factory = sqlite3.Row  # Enable accessing rows by column names
+    return conn
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Endpoint to retrieve all items
+@app.get("/items/")
+def read_items():
+    conn = get_db_conn()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM items')
+    items = cursor.fetchall()
+    conn.close()
+    return items
 
-@app.get("/")
-def read_root():
-    return {"Hello": "From Nejc"}
-
-
+# Endpoint to retrieve a specific item by ID
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    conn = get_db_conn()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM items WHERE id = ?', (item_id,))
+    item = cursor.fetchone()
+    conn.close()
+    return item
